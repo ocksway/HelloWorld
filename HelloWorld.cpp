7c26717c82bfd59a558ab7787a2a4b582d4678ff
Akshay Aggarwal <aggarwaa@usc.edu>
2014-01-15 15:48:56 -0800

    My second commit for HelloWorld

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,13 +1,4 @@
-{\rtf1\ansi\ansicpg1252\cocoartf1265
-{\fonttbl\f0\froman\fcharset0 Times-Roman;}
-{\colortbl;\red255\green255\blue255;\red173\green103\blue5;\red247\green247\blue247;\red158\green0\blue49;
-\red0\green0\blue255;\red38\green38\blue38;\red83\green83\blue83;\red169\green14\blue26;}
-\margl1440\margr1440\vieww10800\viewh8400\viewkind0
-\deftab720
-\pard\pardeftab720\sa240
-
-\f0\fs24 \cf2 \cb3 #include <iostream>\cf0 \
-\pard\pardeftab720\sa240
-\cf4 int \cf5 main\cf6 () \{\uc0\u8232 std\cf7 ::\cf6 cout \cf7 << \cf8 "HelloWorld!" \cf7 << \cf6 std\cf7 ::\cf6 endl;\cf0 \
-\cf6 \}\cf0 \
+#include <iostream>
+int main() {
+    std::cout << "HelloWorld!" << std::endl;
 }
